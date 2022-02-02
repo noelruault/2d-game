2905288af908e23528e6bcf1bd045ba5c3a56f2e
Noël Ruault <noel.ruault@gain.pro>
2022-02-02 21:23:51 +0100

    Change project structure

--- README.md
+++ README.md
@@ -2,15 +2,16 @@
 
 ## Tutorials
 
-Part 1: https://youtu.be/fyi4vfbKEeo
-Part 2: https://youtu.be/bpbghr3NnUU
-Part 3: https://youtu.be/Bu0jT7xmdqI
-Part 4: https://youtu.be/AMHHUIkUX-g
-Part 5: https://youtu.be/Wo9tP6aBmr8
-Part 6: https://youtu.be/928rjpX3Axg
-Part 7: https://youtu.be/diyWzL5bdKY
-Part 8: https://youtu.be/e144CXGy2mc
-Part 9: https://youtu.be/U7fYOnedjzs
+Part 01: [Project beginnings](https://youtu.be/fyi4vfbKEeo)
+Part 02: [Adding Game Objects](https://youtu.be/bpbghr3NnUU)
+Part 03: [Overworld maps](https://youtu.be/Bu0jT7xmdqI)
+Part 04: [Grid based movement](https://youtu.be/AMHHUIkUX-g)
+Part 05: [Character animations](https://youtu.be/Wo9tP6aBmr8)
+Part 06: [Camera](https://youtu.be/928rjpX3Axg)
+Part 07: [Walls & collisions](https://youtu.be/diyWzL5bdKY)
+Part 08: [Behaviors & cutscenes](https://youtu.be/e144CXGy2mc)
+Part 09: [Expanded cutscene behaviors](https://youtu.be/U7fYOnedjzs)
+Part 10: [Typewriters & scene transitions](https://youtu.be/Ot3X2dYSr3Q)
 
 ## Map Dimensions
 
--- js/TextMessage.js
+++ js/TextMessage.js
@@ -7,8 +7,13 @@ class TextMessage {
 
   // done resolves a TextMessage event
   done() {
-    this.element.remove();
-    this.onComplete();
+    if (this.revealingText.isDone) {
+      this.element.remove();
+      this.actionListener.unbind();
+      this.onComplete();
+    } else {
+      this.revealingText.warpToDone();
+    }
   }
 
   createElement() {
@@ -17,9 +22,15 @@ class TextMessage {
     this.element.classList.add("TextMessage");
 
     this.element.innerHTML = (`
-      <p class="TextMessage_p">${this.text}</p>
+      <p class="TextMessage_p"></p>
       <button class="TextMessage_button">Next</button>
     `);
+
+    // Init the typewriter effect
+    this.revealingText = new RevealingText({
+      element: this.element.querySelector(".TextMessage_p"),
+      text: this.text
+    })
 
     this.element.querySelector("button").addEventListener("click", () => {
       // Close the text message
@@ -27,13 +38,13 @@ class TextMessage {
     });
 
     this.actionListener = new KeyPressListener("Enter", () => {
-      this.actionListener.unbind();
       this.done();
     });
   }
 
   init(container) {
     this.createElement();
-    container.appendChild(this.element)
+    container.appendChild(this.element);
+    this.revealingText.init();
   }
 }
